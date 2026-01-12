cc678009de55c34f259494e8622e6b6988ad9d67
NicolasPerego <n.perego@gmail.com>
2026-01-12 18:32:01 +0100

    Update bonobox.sh

--- bonobox.sh
+++ bonobox.sh
@@ -324,8 +324,12 @@ if [ ! -f "$NGINXENABLE"/rutorrent.conf ]; then
 
 	# téléchargement et déplacement de rutorrent
 	"$CMDGIT" clone --progress https://github.com/Novik/ruTorrent.git "$RUTORRENT"
-	cd ruTorrent
-	git checkout v3.8
+	cd "$RUTORRENT" || exit
+	"$CMDGIT" fetch --tags
+	"$CMDGIT" checkout "v3.8" || {
+	    echo "Error: ruTorrent v3.8 tag not found!"
+	    exit 1
+	}
 	cd ..
 	"$CMDECHO" ""; set "146" "134"; FONCTXT "$1" "$2"; "$CMDECHO" -e "${CBLUE}$TXT1${CEND}${CGREEN}$TXT2${CEND}"; "$CMDECHO" ""
 
